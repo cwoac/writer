38c00a74564b021dafdb5e6fa5a576d3a1948c82
Oliver Matthews <djprotoss@hotmail.com>
2013-02-21 17:22:26 +0000

    convert writer.js to a module.

--- scripts/writer.js
+++ scripts/writer.js
@@ -1,15 +1,37 @@
-function logFeature( msg )
+var writer = (function ()
 {
-  var ol = document.getElementById("features");
-  var li = document.createElement("li");
-  li.innerHTML = msg;
-  ol.appendChild(li);
-}
+  // private variable declarations here
+  var cursorLeft;
+  var cursorTop;
 
- function testForFeatures()
- {
-  if( Modernizr.canvas ) logFeature("canvas"); else logFeature("no canvas");
-  if( Modernizr.canvastext ) logFeature("canvas text"); else logFeature("no canvas text");
-  if( Modernizr.indexeddb ) logFeature("indexeddb"); else logFeature("no indexeddb");
+  // handy pointer to canvas element
+  var canvas;
 
- }
+  function logFeature( msg )
+  {
+    var ol = document.getElementById("features");
+    var li = document.createElement("li");
+    li.innerHTML = msg;
+    ol.appendChild(li);
+  }
+
+  function testForFeatures()
+  {
+    if( Modernizr.canvas ) logFeature("canvas"); else logFeature("no canvas");
+    if( Modernizr.canvastext ) logFeature("canvas text"); else logFeature("no canvas text");
+    if( Modernizr.indexeddb ) logFeature("indexeddb"); else logFeature("no indexeddb");
+  }
+
+  // publicly exposed stuff
+  var pub = {};
+
+  pub.initialiseWriter = function()
+  {
+    testForFeatures();
+    canvas = document.getElementById("theCanvas");
+    canvas.width = document.width - 10;
+    canvas.height = document.height - 10;
+  }
+
+  return pub;
+}());
