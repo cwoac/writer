c82e2df5ff64e6165a1ca603aeb262a697a13aff
Oliver Matthews <djprotoss@hotmail.com>
2013-04-18 18:01:09 +0100

    basic lines added

--- scripts/writer.js
+++ scripts/writer.js
@@ -20,7 +20,10 @@ var writer = (function ()
 
   var boxes = [];
   var selectList = [];
+  var lines = [];
 
+
+  // the rubber band for multi-selection
   var band = {};
   band.x=0;
   band.y=0;
@@ -51,7 +54,21 @@ var writer = (function ()
     box.w = 50;
     box.h = 50;
     box.selected = false;
+    box.links = [];
     boxes.push( box );
+  }
+
+  // checks if there is a link from one box to another.
+  // note that links have a direction.
+  function boxIsLinkedTo( from, to )
+  {
+    if( !from || !to ) return false;
+
+    from.links.forEach( function(link){ 
+      if( link==to ) return true;
+    });
+
+    return false;
   }
 
   // selection functions
@@ -141,6 +158,27 @@ var writer = (function ()
     });
   }
 
+  function pickUnselected( e )
+  {
+    var box = null;
+    // loop in reverse order to depth sort
+    for( var i=boxes.length-1; i>=0; i-- )
+    {
+      if(   !boxes[i].selected
+          && e.offsetX>boxes[i].x
+          && e.offsetX<boxes[i].x+boxes[i].w
+          && e.offsetY>boxes[i].y
+          && e.offsetY<boxes[i].y+boxes[i].h)
+      {
+        box = boxes[i];
+        break;
+      }
+    }
+
+    return box;
+  }
+
+
   function pick( e )
   {
     var box = null;
@@ -160,6 +198,24 @@ var writer = (function ()
     return box;
   }
 
+  // line handlers
+
+  // Creates a line from one box to another
+  function addLine( from, to )
+  {
+    // don't allow links to yourself.
+    if( !from || !to || from == to ) return;
+    // only one link per box pair
+    if( boxIsLinkedTo(from,to) ) return;
+
+    var line = {}
+    line.from = from;
+    line.to = to;
+    from.links.push( line );
+    lines.push( line );
+
+  }
+
   // mouse handlers
 
   function mouseUpHandler(e)
@@ -173,6 +229,17 @@ var writer = (function ()
     }
     else
     {
+      // have we done a drag-n-drop?
+      var target = pickUnselected(e);
+      if( target )
+      {
+        //link each selected box to the target and put them back in place.
+        selectList.forEach( function(box){ 
+          addLine(box,target);
+          box.x = box.origX;
+          box.y = box.origY;
+        });
+      }
       // all the work for clicks / drags will have been already completed.
       mouseUpTime = e.timeStamp;
     }
@@ -218,6 +285,10 @@ var writer = (function ()
       selectNone();
 
     selectBox(clickBox);
+    selectList.forEach(function(box){
+      box.origX = box.x;
+      box.origY = box.y;
+    })
     dragOffsetX = e.offsetX;
     dragOffsetY = e.offsetY;
     band.x = dragOffsetX;
@@ -240,6 +311,13 @@ var writer = (function ()
     context.strokeRect( box.x, box.y, box.w, box.h );
   }
 
+  function drawLine( line )
+  {
+    // TODO:: proper edge detection
+    context.moveTo( line.from.x+25, line.from.y+25 );
+    context.lineTo( line.to.x+25, line.to.y+25 );
+  }
+
   function drawBand()
   {
     context.strokeStyle = "#00ff00";
@@ -254,6 +332,9 @@ var writer = (function ()
   {
     // reset the contents of the canvas
     canvas.width = canvas.width;
+    lines.forEach(drawLine);
+    context.strokeStyle = "#0000ff";
+    context.stroke();
     boxes.forEach(drawBox);
     if( band.inUse ) drawBand();
   }
