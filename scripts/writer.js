d638aef050ecd72dbb07029705918d944d4b320d
Oliver Matthews <ojm@codersoffortune.net>
2013-04-19 23:01:19 +0100

    move boxes into a submodule.

--- scripts/writer.js
+++ scripts/writer.js
@@ -18,10 +18,8 @@ var writer = (function (my)
   var clickBoxState=false;
   var clickBox=null;
 
-  var boxes = [];
-  var selectList = [];
+ 
   var lines = [];
-
 
   // the rubber band for multi-selection
   var band = {};
@@ -32,89 +30,13 @@ var writer = (function (my)
   band.inUse = false;
 
 
-  // box manipulation functions
-
-
-  // create a small, empty box.
-  function addBox( e )
-  {
-    var box = {};
-    box.x = e.offsetX-25;
-    box.y = e.offsetY-25;
-    box.w = 50;
-    box.h = 50;
-    box.selected = false;
-    box.links = [];
-    boxes.push( box );
-  }
-
-  // checks if there is a link from one box to another.
-  // note that links have a direction.
-  function boxIsLinkedTo( from, to )
-  {
-    if( !from || !to ) return false;
-
-    from.links.forEach( function(link){ 
-      if( link==to ) return true;
-    });
-
-    return false;
-  }
-
-  // selection functions
-
-
-  function selectNone()
-  {
-    boxes.forEach( function(entry){ entry.selected=false; });
-    selectList = [];
-  }
-
-  function selectBox( box )
-  {
-    // if the box is already selected, do nothin
-    if( box && selectList.indexOf(box) == -1 )
-    {
-      box.selected = true;
-      selectList.push(box);
-    }
-  }
-
-  function selectOnlyBox( box )
-  {
-    selectNone();
-    selectBox(box);
-  }
-
-  function deselectBox( box )
-  {
-    // if the box is already unselected, do nothing
-    if( selectList.indexOf(box) > -1 )
-    {
-      box.selected = false;
-      selectList.splice(selectList.indexOf(box),1);
-    }
-  }
-
-  function toggleSelect( box )
-  {
-    if( selectList.indexOf(box) == -1 )
-    {
-      selectBox(box);
-    }
-    else
-    {
-      deselectBox(box);
-    }
-  }
-
   function selectBand()
   {
     // shouldn't trigger, but hey.
     if( !band.inUse ) return;
 
     // deselect everything as the band may have shrunk
-    selectNone();
+    my.boxes.selectNone();
 
     var top;
     var bottom;
@@ -146,60 +68,15 @@ var writer = (function (my)
 
 
     // every box that is touched by the band is selected
-    boxes.forEach( function(box){
+    my.boxes.forEach( function(box){
       if(    (box.x + box.w >= left && box.x <= right)
           && (box.y + box.h >= top && box.y <= bottom) )
       {
-        selectBox(box)
+        my.boxes.select(box)
       }
     });
   }
 
-  // attempts to pick a box under the cursor
-  // ignores already selected boxes
-  // picks the top-most box it finds
-  // used by drag-n-drop.
-  function pickUnselected( e )
-  {
-    var box = null;
-    // loop in reverse order to depth sort
-    for( var i=boxes.length-1; i>=0; i-- )
-    {
-      if(   !boxes[i].selected
-          && e.offsetX>boxes[i].x
-          && e.offsetX<boxes[i].x+boxes[i].w
-          && e.offsetY>boxes[i].y
-          && e.offsetY<boxes[i].y+boxes[i].h)
-      {
-        box = boxes[i];
-        break;
-      }
-    }
-
-    return box;
-  }
-
-  // attempts to select a box under the cursor
-  // will pick the top-most box it finds, if any
-  // used by click + double-click
-  function pick( e )
-  {
-    var box = null;
-    // loop in reverse order to depth sort
-    for( var i=boxes.length-1; i>=0; i-- )
-    {
-      if(    e.offsetX>boxes[i].x
-          && e.offsetX<boxes[i].x+boxes[i].w
-          && e.offsetY>boxes[i].y
-          && e.offsetY<boxes[i].y+boxes[i].h)
-      {
-        box = boxes[i];
-        break;
-      }
-    }
-
-    return box;
-  }
 
 
   // line handlers
@@ -210,7 +87,7 @@ var writer = (function (my)
     // don't allow links to yourself.
     if( !from || !to || from == to ) return;
     // only one link per box pair
-    if( boxIsLinkedTo(from,to) ) return;
+    if( my.boxes.IsLinked(from,to) ) return;
 
     var line = {}
     line.from = from;
@@ -228,16 +105,16 @@ var writer = (function (my)
         && e.timeStamp - mouseDownTime < 200 )
     {
       mouseUpTime = 0;
-      addBox( e );
+      my.boxes.add( e );
     }
     else
     {
       // have we done a drag-n-drop?
-      var target = pickUnselected(e);
+      var target = my.boxes.pickUnselected(e);
       if( target )
       {
         //link each selected box to the target and put them back in place.
-        selectList.forEach( function(box){ 
+        my.boxes.forEachSelected( function(box){ 
           addLine(box,target);
           box.x = box.origX;
           box.y = box.origY;
@@ -256,12 +133,12 @@ var writer = (function (my)
   // handles drags. Note, not on by default, added/removed by mouseUp/Down Handlers.
   function mouseMoveHandler(e)
   {
-    if( selectList.length>0 && !band.inUse )
+    if( my.boxes.selectedCount() >0 && !band.inUse )
     {
       // we are click-dragging one or more boxes.
       var deltaX = e.offsetX - dragOffsetX;
       var deltaY = e.offsetY - dragOffsetY;
-      selectList.forEach( function(box) {
+      my.boxes.forEachSelected( function(box) {
         box.x += deltaX;
         box.y += deltaY;
       });
@@ -286,17 +163,17 @@ var writer = (function (my)
     mouseIsDrag = false;
 
     // did he click anything?
-    clickBox=pick(e);
+    clickBox=my.boxes.pick(e);
     clickBoxState = clickBox!=null && clickBox.selected;
     
     // if the user clicks an unselected box and doesn't have shift held down deselect anything else.
     if( !event.shiftKey && !clickBoxState )
-      selectNone();
+      my.boxes.selectNone();
 
     // select the clicked box (if any)
-    selectBox(clickBox);
+    my.boxes.select(clickBox);
     // for all selected boxes, save their original location in case we do a drag-n-drop and need to snap back 
-    selectList.forEach(function(box){
+    my.boxes.forEachSelected(function(box){
       box.origX = box.x;
       box.origY = box.y;
     })
@@ -306,7 +183,7 @@ var writer = (function (my)
     dragOffsetY = e.offsetY;
 
 
-    if( selectList.length == 0 ) 
+    if( my.boxes.selectedCount() == 0 ) 
     {
       band.inUse = true;
       band.x = dragOffsetX;
@@ -319,16 +196,7 @@ var writer = (function (my)
 
   // drawing functions
 
-  function drawBox( box )
-  {
-    if( box.selected )
-      context.strokeStyle = "#ff0000";
-    else
-      context.strokeStyle = "#000000";
 
-    // TODO:: sort by stroke style and use .rect and .stroke
-    context.strokeRect( box.x, box.y, box.w, box.h );
-  }
 
   function drawLine( line )
   {
@@ -482,7 +350,7 @@ var writer = (function (my)
     lines.forEach(drawLine);
     context.strokeStyle = "#0000ff";
     context.stroke();
-    boxes.forEach(drawBox);
+    my.boxes.draw(context);
     if( band.inUse ) drawBand();
   }
 
